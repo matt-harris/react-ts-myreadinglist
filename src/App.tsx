2803a18a4a557ee1772a1a68c2cce61b49a3799d
Matt Harris <hello@matt-harris.net>
2020-06-30 22:45:47 +0100

    WIP: start to add light and dark themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
 import ListContextProvider from './contexts/ListContext';
 import Header from './components/Header';
 import List from './components/List';
@@ -6,6 +7,7 @@ import ListForm from './components/ListForm';
 import Footer from './components/Footer';
 import styled from 'styled-components';
 import GlobalStyles from './styles/GlobalStyle';
+import { lightTheme, darkTheme } from './styles/Theme';
 
 const Container = styled.div`
   max-width: 44rem;
@@ -16,19 +18,30 @@ const Container = styled.div`
 `;
 
 function App() {
+  const storedTheme = localStorage.getItem('isDarkMode');
+  const initialThemeState: boolean = storedTheme === 'true' ? true : false;
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(initialThemeState);
+  const handleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+    localStorage.setItem('isDarkMode', JSON.stringify(!isDarkMode));
+  };
+
   return (
     <>
       <GlobalStyles />
-      <div className='App'>
-        <ListContextProvider>
-          <Header />
-          <Container>
-            <List />
-            <ListForm />
-          </Container>
-          <Footer />
-        </ListContextProvider>
-      </div>
+      <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+        <div className='App'>
+          <ListContextProvider>
+            {/* <Button label='Toggle Dark mode' onClick={handleTheme} /> */}
+            <Container>
+              <Header isDarkMode={isDarkMode} onClick={handleTheme} />
+              <List />
+              <ListForm />
+            </Container>
+            <Footer />
+          </ListContextProvider>
+        </div>
+      </ThemeProvider>
     </>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,45 +1,85 @@
 import React, { useContext } from 'react';
+import { ReactComponent as ReadmeLogoSVG } from '../assets/logo.svg';
+import { ReactComponent as SunSVG } from '../assets/sun.svg';
+import { ReactComponent as MoonSVG } from '../assets/moon.svg';
 import styled from 'styled-components';
 import { ListContext } from '../contexts/ListContext';
 
-const Navbar = styled.header`
+const HeaderWrap = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   padding: 1rem 1.5rem;
+`;
+
+const LogoWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-right: 1.5rem;
+`;
+
+const LogoIcon = styled(ReadmeLogoSVG)`
+  fill: #000;
+  width: 2rem;
+  height: 2rem;
+  margin-right: 0.25rem;
+  backface-visibility: hidden;
+  overflow: hidden;
+  vertical-align: middle;
+`;
+
+const LogoText = styled.h1`
+  font-size: 2.25rem;
+  font-weight: 900;
+  text-transform: uppercase;
+`;
+
+const SunIcon = styled(SunSVG)`
+  fill: #000;
+  width: 2rem;
+  height: 2rem;
+  backface-visibility: hidden;
+  overflow: hidden;
+  vertical-align: middle;
+  cursor: pointer;
+`;
+
+const MoonIcon = styled(MoonSVG)`
+  fill: #000;
+  width: 2rem;
+  height: 2rem;
+  backface-visibility: hidden;
+  overflow: hidden;
+  vertical-align: middle;
+  cursor: pointer;
+`;
+
+const HeaderMessage = styled.h3`
+  font-size: 1.25rem;
   text-align: center;
 `;
 
-const Logo = styled.h1`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-bottom: 1.5rem;
-  font-size: 2.5rem;
-  font-weight: 700;
-
-  &:before,
-  &:after {
-    content: '';
-    width: 2rem;
-    border-top: #e3dccf 0.25rem solid;
-  }
-
-  &:before {
-    margin-right: 0.5rem;
-  }
-
-  &:after {
-    margin-left: 0.5rem;
-  }
-`;
-
-const Header = () => {
+const Header = (props: { isDarkMode: boolean; onClick: () => void }) => {
   const { list } = useContext(ListContext);
 
   return (
-    <Navbar>
-      <Logo>MyReadingList</Logo>
-      <p>Currently you have {list?.length} items to read.</p>
-    </Navbar>
+    <>
+      <HeaderWrap>
+        <LogoWrap>
+          <LogoIcon />
+          <LogoText>Readme</LogoText>
+        </LogoWrap>
+
+        {props.isDarkMode ? (
+          <SunIcon onClick={props.onClick} />
+        ) : (
+          <MoonIcon onClick={props.onClick} />
+        )}
+      </HeaderWrap>
+
+      <HeaderMessage>Currently you have {list?.length} items to read.</HeaderMessage>
+    </>
   );
 };
-
 export default Header;
