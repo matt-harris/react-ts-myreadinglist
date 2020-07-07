e7e97c7e88fec94e0efe9616bdff4b9602ff1a45
Matt Harris <hello@matt-harris.net>
2020-07-07 21:48:30 +0100

    Updated light/dark theme colours.

---
+++ src/styles/Theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from 'styled-components';
+
+export const lightTheme: DefaultTheme = {
+  primary: '#d8dee9',
+  secondary: '#eceff4',
+  baseUI: '#434c5e',
+  highlight: '#81a1c1',
+};
+
+export const darkTheme: DefaultTheme = {
+  primary: '#3b4252',
+  secondary: '#4c566a',
+  baseUI: '#e5e9f0',
+  highlight: '#A3be8c',
+};
